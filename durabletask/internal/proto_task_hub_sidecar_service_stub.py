e5487002f027b58a3faf2de08dc8aaf235f54200
Andy Staples <andystaples@microsoft.com>
2025-12-05 12:14:40 -0700

    Use Protocol instead of init with properties

--- durabletask/internal/proto_task_hub_sidecar_service_stub.py
+++ durabletask/internal/proto_task_hub_sidecar_service_stub.py
@@ -1,38 +1,34 @@
-from typing import Any, Callable
+from typing import Any, Callable, Protocol
 
 
-class ProtoTaskHubSidecarServiceStub(object):
+class ProtoTaskHubSidecarServiceStub(Protocol):
     """A stub class roughly matching the TaskHubSidecarServiceStub generated from the .proto file.
     Used by Azure Functions during orchestration and entity executions to inject custom behavior,
     as no real sidecar stub is available.
     """
-
-    def __init__(self):
-        """Constructor.
-        """
-        self.Hello: Callable[..., Any]
-        self.StartInstance: Callable[..., Any]
-        self.GetInstance: Callable[..., Any]
-        self.RewindInstance: Callable[..., Any]
-        self.WaitForInstanceStart: Callable[..., Any]
-        self.WaitForInstanceCompletion: Callable[..., Any]
-        self.RaiseEvent: Callable[..., Any]
-        self.TerminateInstance: Callable[..., Any]
-        self.SuspendInstance: Callable[..., Any]
-        self.ResumeInstance: Callable[..., Any]
-        self.QueryInstances: Callable[..., Any]
-        self.PurgeInstances: Callable[..., Any]
-        self.GetWorkItems: Callable[..., Any]
-        self.CompleteActivityTask: Callable[..., Any]
-        self.CompleteOrchestratorTask: Callable[..., Any]
-        self.CompleteEntityTask: Callable[..., Any]
-        self.StreamInstanceHistory: Callable[..., Any]
-        self.CreateTaskHub: Callable[..., Any]
-        self.DeleteTaskHub: Callable[..., Any]
-        self.SignalEntity: Callable[..., Any]
-        self.GetEntity: Callable[..., Any]
-        self.QueryEntities: Callable[..., Any]
-        self.CleanEntityStorage: Callable[..., Any]
-        self.AbandonTaskActivityWorkItem: Callable[..., Any]
-        self.AbandonTaskOrchestratorWorkItem: Callable[..., Any]
-        self.AbandonTaskEntityWorkItem: Callable[..., Any]
+    Hello: Callable[..., Any]
+    StartInstance: Callable[..., Any]
+    GetInstance: Callable[..., Any]
+    RewindInstance: Callable[..., Any]
+    WaitForInstanceStart: Callable[..., Any]
+    WaitForInstanceCompletion: Callable[..., Any]
+    RaiseEvent: Callable[..., Any]
+    TerminateInstance: Callable[..., Any]
+    SuspendInstance: Callable[..., Any]
+    ResumeInstance: Callable[..., Any]
+    QueryInstances: Callable[..., Any]
+    PurgeInstances: Callable[..., Any]
+    GetWorkItems: Callable[..., Any]
+    CompleteActivityTask: Callable[..., Any]
+    CompleteOrchestratorTask: Callable[..., Any]
+    CompleteEntityTask: Callable[..., Any]
+    StreamInstanceHistory: Callable[..., Any]
+    CreateTaskHub: Callable[..., Any]
+    DeleteTaskHub: Callable[..., Any]
+    SignalEntity: Callable[..., Any]
+    GetEntity: Callable[..., Any]
+    QueryEntities: Callable[..., Any]
+    CleanEntityStorage: Callable[..., Any]
+    AbandonTaskActivityWorkItem: Callable[..., Any]
+    AbandonTaskOrchestratorWorkItem: Callable[..., Any]
+    AbandonTaskEntityWorkItem: Callable[..., Any]
